5d9b66b8775c3fda3d8e5f1da5f5d6a150ff6c19
Eduard <ghiuzaneduard@gmail.com>
2022-11-01 10:55:56 +0200

    added checkOut, remove and add methods

--- models/Cart.java
+++ models/Cart.java
@@ -17,5 +17,35 @@ public class Cart {
         items.set(index, new Item(item));
     }
 
-    
+    public boolean add(Item item) {
+        if (items.contains(item)) {
+            items.add(new Item(item));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void remove(String name) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getName().equalsIgnoreCase(name)) {
+                items.remove(i);
+            }
+        }
+    }
+
+    public String checkOut() {
+        double subtotal = 0;
+        double tax = 13;
+
+        for (int i = 0; i < items.size(); i++) {
+            subtotal += items.get(i).getPrice();
+        }
+        double total = subtotal + subtotal * tax / 100;
+        return
+            "\tRECEIPT\n\n" +
+            "\tSubtotal: $" + subtotal + "\n" +
+            "\tTax: $" + tax + "\n" +
+            "\tTotal: $" + total + "\n";
+        }
 }
