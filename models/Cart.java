89d14e14877e64ed1f01512af4fcf39043a50d51
Eduard <ghiuzaneduard@gmail.com>
2022-11-01 10:44:16 +0200

    added constructor, setter and getter

--- models/Cart.java
+++ models/Cart.java
@@ -1,5 +1,21 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Cart {
+    private ArrayList<Item> items;
+
+    public Cart() {
+        items = new ArrayList<Item>();
+    }
+
+    public Item getItem(int index) {
+        return new Item(items.get(index));
+    }
+
+    public void setItem(Item item, int index) {
+        items.set(index, new Item(item));
+    }
+
     
 }
