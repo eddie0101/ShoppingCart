5a662074f73ba8f9bc9dc8e1fdb635dda0039906
Eduard <ghiuzaneduard@gmail.com>
2022-11-01 10:36:41 +0200

    thrown exceptions for invalid name and price

--- models/Item.java
+++ models/Item.java
@@ -5,6 +5,12 @@ public class Item {
     private double price;
 
     public Item(String name, double price) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("name cannot be null/blank");
+        }
+        if (price < 0) {
+            throw new IllegalArgumentException("price cannot be less than zero");
+        }
         this.name = name;
         this.price = price;
     }
@@ -23,10 +29,20 @@ public class Item {
     }
 
     public void setName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("name cannot be null/blank");
+        }
         this.name = name;
     }
 
     public void setPrice(double price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("price cannot be less than zero");
+        }
         this.price = price;
+    }
+
+    public String toString() {
+        return "Name: " + this.name + "\nPrice: " + this.price + "\n";
     }
 }
