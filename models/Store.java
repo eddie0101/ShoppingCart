ea6061ed159845b6bdc59df52629954d92e1eeeb
Eduard <ghiuzaneduard@gmail.com>
2022-11-15 17:06:52 +0200

    added fields, constructor, getter and setter

--- models/Store.java
+++ models/Store.java
@@ -1,5 +1,18 @@
 package models;
 
 public class Store {
+    private Item[][] items;
+
+    public Store() {
+        items = new Item[7][3];
+    }
+
+    public Item getItem(int row, int column) {
+        return new Item(items[row][column]);
+    }
+
+    public void setItem(Item item, int row, int column) {
+        items[row][column] = new Item(item);
+    }
     
 }
