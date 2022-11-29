5df92b7ab0bb1d6914c521ee50b24e08cd1deba9
Eduard <ghiuzaneduard@gmail.com>
2022-11-29 14:16:10 +0200

    tested if items are added correctly

--- src/test/CartTest.java
+++ src/test/CartTest.java
@@ -1,11 +1,25 @@
 package src.test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.Before;
+import org.junit.Test;
+
+import src.main.models.Cart;
+import src.main.models.Item;
 
 public class CartTest {
+    Cart cart;
     
     @Before
     public void setup() {
-        
+        cart = new Cart();
+        cart.add(new Item("Fanta", 1.99));
+        cart.add(new Item("Pepsi", 1.89));
+    }
+
+    @Test
+    public void itemAddedTest() {
+        assertTrue(cart.contains(new Item("Fanta", 1.99)));
     }
 }
